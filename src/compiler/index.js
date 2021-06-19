e293c14c220d531d23a02cb3c1ba7018329a86dc
HAHA <1497154694@qq.com>
2021-06-19 15:35:54 +0800

    匹配完整个html

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -7,20 +7,51 @@ const startTagClose = /^\s*(\/?)>/; // 匹配标签结束的 >
 const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 
 
+function start (tagName, attrs) {
+  console.log(tagName, attrs, 'start')
+}
+
+function end (tagName) {
+  console.log(tagName, 'end')
+}
+
+function chars (text) {
+  console.log(text, 'text')
+}
+
 function parseHTML (html) {
 
   while (html) {
     let textEnd = html.indexOf('<')
     if (textEnd === 0) {
+      // 首字符是 < 说明是开始标签
+      // 解析开始标签 上面的属性 id="app"
       const startTagMatch = parseStartTag()
-      console.log(startTagMatch)
-      
+      if (startTagMatch) {
+        start(startTagMatch.tagName, startTagMatch.attrs)
+        continue
+      }
+
+      const endTagMatch = html.match(endTag)
+      if (endTagMatch) {
+        advance(endTagMatch[0].length)
+        end(endTagMatch[1])
+        continue
+      }
     }
 
-    break
+    let text
+    //  hello world </div>  这样情况下  就是 textEnd > 0 匹配是文本
+    if (textEnd >= 0) {
+      text = html.substring(0, textEnd)
+    }
+    if (text) {
+      advance(text.length)
+      chars(text)
+    }
   }
 
-  // 截取字符串，更新 html 内容
+  // 截取字符串，更新 html 内容  每次匹配完，就把匹配到结果从 html 剔除掉，减少下次匹配难度
   function advance (n) {
     html = html.substring(n)
   }
@@ -52,5 +83,5 @@ function parseHTML (html) {
 
 
 export function compileToFunctions(template) {
-  const ast = parseHTML(template)
+  parseHTML(template)
 }
